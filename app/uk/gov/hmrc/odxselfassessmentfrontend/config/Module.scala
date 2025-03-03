b61c76bf2cb7c258568afccc8907ef509db8a0c6
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2025-03-03 13:29:20 +0000

    'Creating new service odx-self-assessment-frontend'

---
+++ app/uk/gov/hmrc/odxselfassessmentfrontend/config/Module.scala
@@ -0,0 +1,11 @@
+package uk.gov.hmrc.odxselfassessmentfrontend.config
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+
+  override def configure(): Unit = {
+
+    bind(classOf[AppConfig]).asEagerSingleton()
+  }
+}
---
+++ conf/messages.cy
@@ -0,0 +1,2 @@
+service.name = odx-self-assessment-frontend
+service.text = Dyma'ch gwasanaeth newydd
